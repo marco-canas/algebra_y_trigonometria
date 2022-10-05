39c6a6c4cc391d9110bf23f4db454845180f3008
Marco Julio Cañas Campillo <66282835+marco-canas@users.noreply.github.com>
2022-10-05 16:38:29 -0500

    adición de corrección a cuaderno modelación de medición de alturas utilizando la visual

--- classes/medicion/medicion_visual.py
+++ classes/medicion/medicion_visual.py
@@ -1,10 +1,11 @@
 # Aquí voy a construir una función que estime la altura de un pararayos utilizando la línea visual
 def medir_altura_visual(altura_persona, dpa, dap, altura_arbol):
     from sympy import var, Eq, solve 
+    
     h, x = var("h, x") 
     ecuacion_1 = Eq(h/altura_arbol, (x+dpa+dap)/(dpa+x) )
     ecuacion_2 = Eq(altura_arbol/altura_persona, x/(x+dpa)) 
-    altura_estimada, x = solve([ecuacion_1, ecuacion_2], (h, x))
+    altura_estimada, x = solve([ecuacion_1, ecuacion_2], (h, x))[0]
     return altura_estimada 
 
 
